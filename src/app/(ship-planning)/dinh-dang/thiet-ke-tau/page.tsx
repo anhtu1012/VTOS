745b33741ca4bbc515c0e8e67ca0ae22d6796419
phamanhtu1012@gmail.com <phamanhtu1012@gmail.com>
2024-10-11 09:56:34 +0700

    update layout

---
+++ src/app/(ship-planning)/dinh-dang/thiet-ke-tau/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ShipDesign() {
+  return <div>ShipDesign</div>;
+}
+
+export default ShipDesign;
---
+++ src/app/(ship-planning)/dinh-dang/trong-luong-toi-da/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WeightMax() {
+  return <div>WeightMax</div>;
+}
+
+export default WeightMax;
